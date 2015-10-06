ee7589ce28e44e149795c00918f31e133439f027
peter <peter.k.nets@gmail.com>
2015-10-06 13:03:21 +0300

    Add controller to 'Notes' route

--- app/js/controllers/notes-index-controller.js
+++ app/js/controllers/notes-index-controller.js
@@ -1,14 +1,7 @@
-angular.module('NoteWrangler').controller('NotesIndexController', function($scope, Note, Session) {
-  // Without NgResource
-  // Note.all().success(function(data) {
-  //   $scope.notes = data;
-  // });
-  
-  // With NgResource
-  $scope.notes = Note.query();
-
-  Session.sessionData().success(function(sessionUser) {
-    // Create a new User from the session user data
-    $scope.loggedIn = !!sessionUser;
-  });
-});
+angular.module('NoteWrangler')
+    .controller('NotesIndexController', function($http){
+        var controller = this;
+        $http({method: 'GET', url: '/notes'}).success(function(data){
+            controller.notes = data;
+        });
+    });
--- app/js/routes.js
+++ app/js/routes.js
@@ -2,7 +2,9 @@ angular.module('NoteWrangler')
     .config(function($routeProvider){
         $routeProvider
             .when('/notes', {
-                templateUrl: '/template/pages/notes/index.html'
+                templateUrl: '/template/pages/notes/index.html',
+                controller: 'NotesIndexController',
+                controllerAs: 'indexController'
             })
             .when('/users', {
                 templateUrl: '/template/pages/users/index.html'
